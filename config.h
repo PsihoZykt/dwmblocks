6648f1e8fde3d6858d1e7c8b86cc8aca13c6683f
psihozykt <psihozykt@gmail.com>
2021-07-19 21:44:53 +0900

    Update: 2021-07-19 12:44:53

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 {"", "kbselect", 0, 30},
